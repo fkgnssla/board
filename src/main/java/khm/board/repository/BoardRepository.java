b6e554925fc8df8af2f2cf5b63ee4f927378ed2c
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2022-08-11 20:32:57 +0900

    [feat]: 글 생성, 게시글 리스트 뷰 생성

---
+++ src/main/java/khm/board/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package khm.board.repository;
+
+import khm.board.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
